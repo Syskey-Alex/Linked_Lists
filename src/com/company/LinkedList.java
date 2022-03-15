a625d8b6885048b3dc4e5e715037c06f4a046680
Syskey-Alex <alexionita0207@yahoo.com>
2022-03-15 10:17:17 +0000

    some stuff

--- src/com/company/LinkedList.java
+++ src/com/company/LinkedList.java
@@ -9,12 +9,15 @@ public class LinkedList {
 
     public LinkedList() {
         head = null;
+        cursor = null;
     }
-//The method LinkedList() loops through the list, creates a new node, links it to the previous value node, then takes another value and repeats the process again
-    public LinkedList(int[] values){
+
+    //The method LinkedList() loops through the list, creates a new node, links it to the previous value node, then takes another value and repeats the process again
+    public LinkedList(int[] values) {
         head = new Node(values[0]);
+        cursor = head;
         Node n = head;
-        for(int i = 1;i<values.length; i++) {
+        for (int i = 1; i < values.length; i++) {
             //this is a new node (called adding) that has the value we want.
             Node adding = new Node(values[i]);
             //this is setting the next link from previous node which links the new node into the linked-list chain.
@@ -26,11 +29,12 @@ public class LinkedList {
 
     public void addItem(int v) {
         Node n = new Node(v);
-    //this is to make sure that the head isn't pointing to each node individually and that the linked list always refers back to the chain of command.
-       //we need to do this iteratively therefore we use a while loop
+        //this is to make sure that the head isn't pointing to each node individually and that the linked list always refers back to the chain of command.
+        //we need to do this iteratively therefore we use a while loop
         Node current = head;
         if (head == null) {
-            head= n;
+            head = n;
+            cursor = head;
         } else {
             //make the list parse to the end
             current = head;
@@ -45,30 +49,48 @@ public class LinkedList {
 
 
     }
+
     //TODO fix hasNext so that its a boolean and that it checks whether the pointer is at the value.
-public boolean hasNext() {
-    Iterator itr = list.iterator();
+    public boolean hasNext() {
+        //returns as true if there is a valid node at the cursor.
+        return (cursor != null);
+
+        Iterator itr = myList.iterator();
         while (itr.hasNext()) {
             System.out.println(itr.next());
         }
 
-    return false;
-}
-public int size() {
+        return false;
+    }
+
+    public int size() {
         int myList = myList.size();
-        for(int i = 0; i <items.length; i ++) {
+        for (int i = 0; i < items.length; i++) {
             //printing size of the array
             System.out.println();
         }
-}
-public int getNext() {
+        return myList;
+    }
+
+    public int getNext() {
+        int v = cursor.getValue();
         cursor = cursor.getNext();
-        return cursor.getValue();
+        return v;
+    }
+
+    public void resetNext() {
+        cursor = head;
+    }
+
+    //tells you that there is nothing in the list and can save you from going to the specified value in the list and finding out that there is nothing there.
+    public boolean isEmpty() {
+//return true if the list has no elements
+        return (head == null);
     }
 }
 
 
-
+//massive code dump over here:
 
 
 
